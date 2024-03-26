892da7c769dddff2fbf8ade595519617e1993fc5
VitaAtiv <142582763+VitaAtiv@users.noreply.github.com>
2024-03-26 17:23:55 +0200

    add

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 import './style.css'
-
+import { Notify } from "notiflix/build/notiflix-notify-aio";
+Notify.init({
+  position: right-bottom,
+})
+console.log(Notify.success);
+Notify.success("Hello")
 const btn = document.querySelector(".btn");
 btn.addEventListener("click", () => {
-  console.log("hi");
+  
+  Notify.success("Hello");
 })
 
